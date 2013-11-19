dd69f81112e22e32f0c567b62feedd9aa640cffd
CzacasticJack <joshua.weaver@colorado.edu>
2013-11-18 16:15:30 -0800

    Initial commit

---
+++ Derp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derp::Application.config.secret_token = '6fa664b7b00a120ed3a81bc924bed73ea1bd2d18e783a312000052896b88db0b74b31cec51192a2a6780cc5dff0ade399d7b4a7f3104e737ff1f85f818387084'
